ff7a79da214b07361e5440fcfcd927ca56ebf766
Paul Schneider <paulschneider@free.fr>
2018-12-02 17:07:28 +0000

    fixes a missing line ending

--- ifaddr/src/main.c
+++ ifaddr/src/main.c
@@ -7,7 +7,7 @@ int main(int argc, char ** argv)
 {
 
   if (argc<2) {
-    fprintf(stderr, "usage: %s <ifname>\nOù `ifname` est le nom de l'interface, par exemple, `eth0`.", argv[0]);
+    fprintf(stderr, "usage: %s <ifname>\nOù `ifname` est le nom de l'interface, par exemple, `eth0`.\n", argv[0]);
     
     return 2;
   }
